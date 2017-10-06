1b7c98218848d8eb09cf87b9e9d8613bd29ea299
Carlos Mata <carlosmfr9@gmail.com>
2017-10-06 16:10:45 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "about" => "pages#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-devise_for :admins
+
 mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 end
