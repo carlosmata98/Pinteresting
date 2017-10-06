dc8a4f86fb90c859dbf8ba885bcced93c827fa4e
Carlos Mata <carlosmfr9@gmail.com>
2017-10-06 16:06:22 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
-Rails.application.routes.draw do
-  get 'pages/home'
-  get "about" => "pages#about"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
 devise_for :admins
 mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 end
