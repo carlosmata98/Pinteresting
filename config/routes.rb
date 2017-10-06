71191b4ec51a49acc2bb7e907b2241527a939e0b
Carlos Mata <carlosmfr9@gmail.com>
2017-10-06 15:59:01 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+devise_for :admins
+mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+end
