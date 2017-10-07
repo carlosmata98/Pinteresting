b3bf29d1b10145aa87332cbedac12a7dcd5b1aae
Carlos Mata <carlosmfr9@gmail.com>
2017-10-07 11:47:59 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get "about" => "pages#about"
-  resources :users, only: [:index,]
+  resources :users, only: [:index]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
