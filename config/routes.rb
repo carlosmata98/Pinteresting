53ec006fe55b331efffa668b5c6462e9bed48a02
Carlos Mata <carlosmfr9@gmail.com>
2017-10-09 11:28:24 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get "pages/home"
   get "about" => "pages#about"
-  get 'pinteresting/index' => 'index' 
+  root 'pinteresting#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
