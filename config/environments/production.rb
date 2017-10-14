4c338600203ee44ff6a7b2e31e27e339a212aa1d
Carlos Mata <carlosmfr9@gmail.com>
2017-10-13 21:33:15 -0400

    Add amazon s3 for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
 
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  s3_region: 'us-east-2'
+  :storage => :s3, 
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    s3_region: ENV.fetch('AWS_REGION')
   }
 }
   # Use a different logger for distributed setups.
