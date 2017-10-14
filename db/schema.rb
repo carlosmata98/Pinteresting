584b74dedd3f0618d8a5eafb5db99b07666c55f6
Carlos Mata <carlosmfr9@gmail.com>
2017-10-14 12:00:24 -0400

    Added jQuery masonry

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171013224813) do
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
-
+    
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
