84fd2a0d14f91939ca6e414595c0bbaeab61727b
Carlos Mata <carlosmfr9@gmail.com>
2017-10-18 17:03:21 -0400

    a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require bootstrap
 //= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require_tree .
